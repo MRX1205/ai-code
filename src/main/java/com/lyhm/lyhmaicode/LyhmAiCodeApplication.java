02c634c1fe02dae5836627f350ed63f87ccf5509
MRX1205 <2732184038@qq.com>
2026-02-14 10:38:16 +0800

    第一章2：备份

--- src/main/java/com/lyhm/lyhmaicode/LyhmAiCodeApplication.java
+++ src/main/java/com/lyhm/lyhmaicode/LyhmAiCodeApplication.java
@@ -1,13 +1,14 @@
 package com.lyhm.lyhmaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.lyhm.lyhmaicode.mapper")
 public class LyhmAiCodeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LyhmAiCodeApplication.class, args);
     }
